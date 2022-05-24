43405db96cda7bdae25a94759220fe12f1a05ac3
Adam3192 <jeremyadixon@hotmail.com>
2022-05-24 08:59:25 -0500

    React lesson 1 assignment

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 
 const name = React.createElement('h1', null, 'Adam Dixon');
 const title = React.createElement('p', null, 'Aspiring Software Developer');
-const header = React.createElement('div', null, [name, title]);
+const header = React.createElement('div', {id: "header"}, [name, title]);
 
 const attributeArray = ["Quick Learner", "Hard-working", "Loves Coffee"];
 const attributes = attributeArray.map((attribute) => React.createElement('li', null, attribute));
 const attributeList = React.createElement('ul',null, attributes);
 
-const link = React.createElement('a', {href: "https://www.youtube.com/"}, 'My Github Page');
-const footer = React.createElement('div', null, link);
+const link = React.createElement('a', {href: "https://github.com/Adam3192"}, 'My Github Page');
+const footer = React.createElement('div', {id: "footer"}, link);
 
 const container = React.createElement('div', null, [header, attributeList, footer]);
 
